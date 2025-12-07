5615df3f4de3c62bcfd3124162acd5781cf71e57
jojeho <inference_to@naver.com>
2025-12-07 18:37:26 +0900

    Remove 'telescope' from the package list

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #  Dotfiles Main Installer
 # =============================================================================
 
-PACKAGES=('gcc' 'stow' 'neovim' 'tmux' 'xclip' 'fzf' 'fzf-git' 'delta' 'eza' 'bat' 'thefuck' 'telescope' 'gemini-cli') # 색상 유틸리티
+PACKAGES=('gcc' 'stow' 'neovim' 'tmux' 'xclip' 'fzf' 'fzf-git' 'delta' 'eza' 'bat' 'thefuck' 'gemini-cli') # 색상 유틸리티
 
 REPO_URL="https://github.com/jojeho/dotfiles.git"
 DOTFILES_DIR="$HOME/.dotfiles"
