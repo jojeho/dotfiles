77651b7a0b1703eff20cad273d610ce92e4821c1
jojeho <inference_to@naver.com>
2025-12-08 21:25:15 +0900

    Comment out PACKAGES array in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #  Dotfiles Main Installer
 # =============================================================================
 
-#PACKAGES=( 'gcc'  'neovim' 'zellij'  'fzf'  'delta' 'eza' 'bat' 'thefuck'  'gemini-cli' ) # 색상 유틸리티
+#PACKAGES=( 'gcc'  'neovim' 'zellij'  'fzf'  'delta' 'bat' 'thefuck'  'gemini-cli' ) # 색상 유틸리티
 REPO_URL="https://github.com/jojeho/dotfiles.git"
 DOTFILES_DIR="$HOME/.dotfiles"
 
